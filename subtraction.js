54b9c13fd8ba0f03a6f11219a5f88b39f442d7a3
Kiran TC <kiran>
2022-04-13 23:30:35 -0700

    commit

---
+++ subtraction.js
@@ -0,0 +1,8 @@
+function subtract()
+{
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result  = a-b
+document.getElementById("result").value = result
+}
